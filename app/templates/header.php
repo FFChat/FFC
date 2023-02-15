4fe32cef2a802e1eefe5897dcf9d3ec19ec550e5
ALIX Lubin <lubinclg@gmail.com>
2023-02-15 16:54:29 +0100

    rename home -> app

---
+++ app/templates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="bg-dark bg-gradient">
+<head>
+  <meta charset="utf-8" />
+  <title>Untilted</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg navbar- bg-dark"  data-bs-theme="dark">
+
+  <a href="index.php" class="navbar-brand">
+    <img src="images/logo.png" alt="logo" width="75" height="70">
+    FFC
+  </a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">Conso</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a href="index.php?page=essence" class="dropdown-item">Essence</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a href="index.php?page=battle" class="nav-link">Bagarre</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<body>
