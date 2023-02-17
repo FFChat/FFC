000ed30426369b22650da416d15dcaa7eb3f7e3b
Solène Jouen <solenej20@gmail.com>
2023-02-17 12:27:28 +0100

    page login

--- app/page/login.php
+++ app/page/login.php
@@ -1,18 +1,31 @@
-<?php
-  include_once('../templates/header.php')
-?>
+<!DOCTYPE html>
+<html class="">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Untilted</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="/FFC/app/templates/fontawsome/css/fontawesome.css" rel="stylesheet">
+  <link href="/FFC/app/templates/fontawsome/css/brands.css" rel="stylesheet">
+  <link href="/FFC/app/templates/fontawsome/css/solid.css" rel="stylesheet">
+  <link href="/FFC/app/page/css/general.css" rel="stylesheet">
+  <link rel="stylesheet" href="/FFC/app/page/css/login.css">
+</head>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-6">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-    </div>
+  <div class="row align-items-center justify-content-center text-center" style="min-height: 85vh">
+      <div class="col-6 x">
+        <h1 class="text-white">Login</h1>
+          <form>
+            <div class="mb-3"><input type="text" name="u" placeholder="Username" required="required"/></div>
+            <!-- <div class="mb-3"><input type="password" name="p" placeholder="Password" required="required"/></div> -->
+            <div><button type="submit" class="btn btn-primary btn-block btn-large">Come in.</button></div>
+          </form>
+      </div>
+  </div>
 </div>
 
 <?php
   include_once('../templates/footer.php')
 ?>
-<!-- iejfhzifh -->
