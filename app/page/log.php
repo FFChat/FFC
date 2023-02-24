ee8b80aa165692ac7f6b13d3e2f0c61e548cd9e2
Solène Jouen <solenej20@gmail.com>
2023-02-24 11:38:30 +0100

    log

--- app/page/log.php
+++ app/page/log.php
@@ -9,7 +9,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="css/log.css">
-  <link rel="stylesheet" href="js/log.js">
 </head>
 
 <body>
@@ -86,6 +85,21 @@
     </div>
   </section>
 </body>
+
+<script>
+    function switch_etat() {
+        let login = document.getElementById("login");
+        let sign_up = document.getElementById("sign_up");
+
+        if(login.hidden) {
+            login.hidden = false;
+            sign_up.hidden =true;
+        } else {
+            login.hidden = true;
+            sign_up.hidden =false;
+        }
+    }
+</script>
 
 </html>
 
