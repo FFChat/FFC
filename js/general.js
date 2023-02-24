70d1fbcb53cd155b892970fd0dbc73e803f5c786
Gwadaw <Wil.rimb@gmail.com>
2023-02-24 15:04:23 +0100

    mise en place du serveur socket.io

--- js/general.js
+++ js/general.js
@@ -1,3 +1,7 @@
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 function throwNewMessage(id, message, user) {
 
@@ -9,3 +13,23 @@ function throwNewMessage(id, message, user) {
   list.appendChild(element);
 
 }
+
+io.on('connection', (socket) => {
+  const sid = socket.id;
+  
+console.log(`User connected with socket id ${sid}`);
+socket.on('disconnect', () => {
+    console.log(`User with socket id ${sid} disconnected`);
+});
+
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+
+socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
--- serveur.js
+++ serveur.js
@@ -13,17 +13,19 @@ io.on('connection', (socket) => {
   const sid = socket.id;
 
   console.log(`User connected with socket id ${sid}`);
+  io.emit('chat message', `User with socket id ${sid} connected`);
+  
   socket.on('disconnect', () => {
     console.log(`User with socket id ${sid} disconnected`);
+    io.emit('chat message', `User with socket id ${sid} disconnected`);
   });
   
   io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
   
   socket.on('chat message', (msg) => {
-      console.log('')
       io.emit('chat message', msg);
     });
-  });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
