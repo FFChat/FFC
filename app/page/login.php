b639d8a66827c7a41a384b9426e25123f854143e
Solène Jouen <solenej20@gmail.com>
2023-02-24 17:23:36 +0100

    Page login

--- app/page/login.php
+++ app/page/login.php
@@ -8,24 +8,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/login.css">
+  <script src="../../js/login.js"></script>
 </head>
-
-<style>
-body {
-  background-color: rgb(57, 57, 57);
-  background-image: url("../images/logo.png");
-  background-repeat: no-repeat;
-  background-position-x: -50px;
-  background-size: 752px 756px;
-}
-</style>
 
 <body>
   <section id="login">
     <div class="container py-5 h-90">
       <div class="row d-flex justify-content-center align-items-center">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card bg-dark text-white card-a" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
 
               <div class="mb-md-1 mt-md-4 pb-5">
@@ -40,7 +32,7 @@ body {
                   <input type="password" class="form-control form-control-lg" placeholder="Password" />
                 </div>
 
-                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                <button class="btn btn-outline-light btn-lg px-5 btn-a" type="submit">Login</button>
 
               </div>
               <div>
@@ -60,7 +52,7 @@ body {
     <div class="container py-5 h-90">
       <div class="row d-flex justify-content-center align-items-center">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card bg-dark text-white card-a" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
 
               <div class="mb-md-1 mt-md-4 pb-5">
@@ -79,7 +71,7 @@ body {
                   <input type="email" class="form-control form-control-lg" placeholder="Email address" />
                 </div>
 
-                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                <button class="btn btn-outline-light btn-lg px-5 btn-a" type="submit">Login</button>
 
               </div>
               <div>
@@ -94,21 +86,6 @@ body {
     </div>
   </section>
 </body>
-
-<script>
-function switch_etat() {
-  let login = document.getElementById("login");
-  let sign_up = document.getElementById("sign_up");
-
-  if (login.hidden) {
-    login.hidden = false;
-    sign_up.hidden = true;
-  } else {
-    login.hidden = true;
-    sign_up.hidden = false;
-  }
-}
-</script>
 
 </html>
 
