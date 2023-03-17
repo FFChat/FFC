6afbd1e8c1057a42daf269fae81fedd52b411606
ALIX Lubin <lubinclg@gmail.com>
2023-03-17 09:51:54 +0100

    lien footer

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -1,9 +1,9 @@
         <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script> -->
-        <script src="/app/public/vendor/bootstrap/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
-        <!-- <script src="https://cdn.socket.io/4.5.4/socket.io.min.js"></script> -->
-        <script src="/app/public/js/general.js"></script>
+        <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://cdn.socket.io/4.5.4/socket.io.min.js"></script>
+        <script src="/js/general.js"></script>
         <!-- <script src="http://localhost:3000/socket.io/socket.io.js"></script> -->
-        <script src="/app/js/<?= $page ?>.js"></script>
+        <script src="/js/<?= $page ?>.js"></script>
       </main>
     </div>
   </div>
