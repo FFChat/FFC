03702cb310b9bd673621b93c891d17c8d9d21c96
ALIX Lubin <lubinclg@gmail.com>
2023-04-13 12:33:46 +0200

    Message + Header

--- app/templates/header.php
+++ app/templates/header.php
@@ -127,16 +127,9 @@
           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
             id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="#" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>mdo</strong>
+            <strong>UserName</strong>
           </a>
           <ul class="dropup-menu dropdup-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
           </ul>
         </div>
 
--- app/templates/message.php
+++ app/templates/message.php
@@ -1,7 +1,7 @@
 
 
 <li class="general-chat d-flex flex-row border-white border-top p-2 mx-3">
-  <img src="images/img_util/<?= $message['user_id'] ?>.png" id='<?= $message['user_id'] ?>_img' width="32" height="32" class="rounded me-2" alt="image utilisateur">
+  <img src="https://api.dicebear.com/6.x/pixel-art/png?seed=<?= $message['user_id'] ?>" id='<?= $message['user_id'] ?>_img' width="32" height="32" class="rounded me-2" alt="image utilisateur">
   <div>
     <label for="<?= $message['user_id'] ?>_img" class="pb-2 text-white-50"><?= $message['nickname'] ?></label>
     <p><?= $message['text'] ?></p>
